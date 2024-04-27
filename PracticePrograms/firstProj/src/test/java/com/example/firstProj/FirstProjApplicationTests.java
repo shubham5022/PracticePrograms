f117ebbb031f93c9d05b5e7fd6e4b1cd5d2dab20
shubham5022 <92208084+shubham5022@users.noreply.github.com>
2024-04-27 10:59:17 +0530

    Add files via upload

---
+++ PracticePrograms/firstProj/src/test/java/com/example/firstProj/FirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
